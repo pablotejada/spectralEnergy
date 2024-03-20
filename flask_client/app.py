074bf0f1749bf575b0a6b709198dba8f1c8ee305
pablotejada <pablo.tejada@atherasanalytics.com>
2024-03-20 19:09:19 +0000

    Missing README.md

--- flask_client/app.py
+++ flask_client/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, render_template
 import grpc
 import os
 import sys
+import logging
 
 app = Flask(__name__)
 
@@ -11,14 +12,22 @@ def index():
 
 @app.route('/data', methods=['GET'])
 def get_data():
-    response = grpc_stub.GetMeterUsage(meterusage_pb2.Timestamp())
-    data = [{"timestamp": item.timestamp, "usage": item.usage} for item in response]
-    # data = {
-    #     "k1": 1, 
-    #     "k2": 2, 
-    #     "k3": 3,       
-    # }
-    return jsonify(data)
+    """
+    Returns data in json calling the gRPC server
+    """
+    try:
+        with grpc.insecure_channel("localhost:50051") as channel:
+            stub = meterusage_pb2_grpc.MeterUsageServiceStub(channel)
+            response_stream = stub.GetMeterUsage(meterusage_pb2.GetMeterUsageRequest())
+            data = [item.csv_content for item in response_stream]
+        return jsonify(data)
+    except Exception as e:
+        """
+        Add logic on exception:
+            - logging
+            - escalating up the stack and deal accordingly
+        """
+        pass
 
 if __name__ == '__main__':
     # Get the absolute path of the current directory
@@ -35,7 +44,9 @@ if __name__ == '__main__':
         sys.path.append(folder0_path)
 
     from grpc_server import meterusage_pb2, meterusage_pb2_grpc
-    grpc_channel = grpc.insecure_channel('localhost:50051')  # Assuming gRPC server is running locally on port 50051
-    grpc_stub = meterusage_pb2_grpc.MeterUsageServiceStub(grpc_channel)
+
+
+    # grpc_channel = grpc.insecure_channel('localhost:50051')  # Assuming gRPC server is running locally on port 50051
+    # grpc_stub = meterusage_pb2_grpc.MeterUsageServiceStub(grpc_channel)
     
     app.run(debug=True)
